f42131b068f9389ceb378d661114897fc3c6dc1e
zklgola <zklgola@163.com>
2018-06-11 11:17:21 +0800

    add http_response

--- http_request_header.cpp
+++ http_request_header.cpp
@@ -1,19 +1,9 @@
 #include "http_request_header.h"
 
-Http_request_header:: Http_request_header(): m_status(-1), m_status_string("")
+Http_request_header::Http_request_header()
 {
+}
 
-}
-Http_request_header::Http_request_header(char* buffer): Http_header(buffer)
-{
-		string line = Get_first_line();
-		
-		m_status = -1;
-	
-}
 Http_request_header::~Http_request_header()
 {
-
 }
-
-
--- http_request_header.h
+++ http_request_header.h
@@ -1,35 +1,12 @@
-#ifndef HTTP_REQUEST_HEADER_H
-#define HTTP_REQUEST_HEADER_H
+#ifndef HTTP_REQUEST_HEADER
+#define HTTP_REQUEST_HEADER
 
-#include "http_request.h"
+#include "http_header.h"
 
-class Http_request_header: public Http_header
+class Http_request_header:public Http_header
 {
 public:
 	Http_request_header();
-	Http_request_header(char* buffer);
-	~Http_request_header();
-
-	int Get_status()
-	{
-		return m_status;
-	}
-	void Set_status(int value)
-	{
-		m_status = value;
-	}
-	string Get_status_string()
-	{
-		return m_status_string;
-	}
-	void Set_status_string(string value)
-	{
-		m_status_string = value;
-	}
-private:
-	int m_status;
-	string m_status_string;
-};
-
-
-#endif
+	virtual ~Http_request_header();
+}
+#endif 
