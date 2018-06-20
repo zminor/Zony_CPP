785ba040e8a030a0b1aff7a7fdbb5479a08eef87
zminor <zklgola@163.com>
2018-06-21 03:00:36 +0800

    add SharedMemory in processy

---
+++ src/system/SharedMemory.cpp
@@ -0,0 +1,4 @@
+#include "SharedMemory.h"
+
+
+
---
+++ src/system/SharedMemory.h
@@ -0,0 +1,45 @@
+#ifndef __SHAREDMEMORY_H__
+#define __SHAREDMEMORY_H__
+
+#include <string>
+namespace System
+{
+	class SharedMemory
+	{
+	public:
+		SharedMemory() noexcept;
+		~SharedMemory() noexcept;
+
+		bool create(
+			const std::string &memName,
+			const size_t memSize
+			);
+
+		bool open(const std::string &memName);
+
+		bool is_open() const noexcept;
+
+		bool write(
+			const void *data,
+			const size_t size,
+			const size_t offset =0
+		)const noexcept;
+		
+		bool read(
+			void *dest,
+			const size_t size,
+			const size_t offset = 0
+		)const noexcept;
+		
+		bool close() noexcept;		
+		bool destroy();
+
+		static bool destroy(const std::string &memName);
+
+	private:
+		int shm_desc;
+		std::string shm_name;
+	};
+}
+
+#endif
