d60740104c48438a025c3ac2c4bff8728e6c61a6
zminor <zklgola@163.com>
2018-06-24 14:36:59 +0800

    add ServerControl pass Compilation

--- src/server/ServerControls.cpp
+++ src/server/ServerControls.cpp
@@ -17,30 +17,57 @@ namespace HttpServer
 
 	void ServerControls:: clear()
 	{
-	
+		if(this->eventProcessQueue)
+		{
+			delete this->eventProcessQueue;
+			this->eventProcessQueue = nullptr;
+		}
+
+		if (this->eventProcessQueue) 
+		{
+			delete this->eventProcessQueue;
+			this->eventProcessQueue = nullptr;
+		}
+
+		if (this->eventUpdateModule) {
+			delete this->eventUpdateModule;
+			this->eventUpdateModule = nullptr;
+		}
 	}
 
 	void ServerControls::setProcess(const bool flag)
 	{
+					this->process_flag = flag;
 	}
 
 	void ServerControls::stopProcess()
 	{
-	
+					this->process_flag = false;
+					if(this->eventNotFullQueue)
+					{
+						this->eventNotFullQueue->notify();
+					}
+					this->setProcessQueue();
 	}
 
 	void ServerControls::setRestart(const bool flag)
 	{
-	
+		this->restart_flag = flag;
 	}
 
 	void ServerControls::setUpdateModule()
 	{
-	
+		if(this->eventUpdateModule)
+		{
+			this->eventUpdateModule->notify();
+		}
 	}
 
 	void ServerControls::setProcessQueue()
 	{
-	
+			if(this->eventProcessQueue)
+			{
+				this->eventProcessQueue->notify();
+			}
 	}
 }
