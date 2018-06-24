200decb840522b1388e5da448ed388e73c8ccd49
zminor <zklgola@163.com>
2018-06-24 20:57:44 +0800

    add http2 & SocketQueue

---
+++ src/server/SocketsQueue.h
@@ -0,0 +1,25 @@
+#ifndef __SOCKETSQUEUE_H__
+#define __SOCKETSQUEUE_H__
+
+#include "../socket/Socket.h"
+#include "../system/Cache.h"
+
+#include "../transfer/http2/Http2.h"
+
+#include <mutex>
+#include <queue>
+
+namespace HttpServer
+{
+
+	class SocketsQueue:
+				public std:queue<std::tuple <Socket::Socket,Http2::IncStream *>>,
+				System::CachePadding<std::queue<std::tuple <Socket::Socket, Http2::IncStream *>>>,
+				public std::mutex
+				{
+				
+				};
+
+}
+
+#endif
