079926766654a8f72838c497fec34eb98c4b7472
zklgola <zklgola@163.com>
2018-06-26 17:44:57 +0800

    add UrlEncoded & MultiPartFormData

---
+++ src/server/data-variant/FormUrlencoded.cpp
@@ -0,0 +1,90 @@
+#include "FormUrlencoded.h"
+
+namespace DataVariant
+{
+	FormUrlencoded :: FormUrlencoded () noexcept
+	{
+		this->data_variant_name = "application/x-www-form-urlencoded";
+	}
+
+	bool FormUrlencoded :: parse(
+		const std::string &buf,
+		Transfer::request_data *rd.
+		DataReceiver *dr) const 
+	{
+		if (buf.empty() ) 
+		{
+			return 0 == dr->full_size || dr->full_size != dr->recv_total;
+		}
+
+		for (
+			size_t var_pos = 0, var_end = 0;
+			std::string::npos != var_end;
+			var_pos = var_end + 1	) 
+		{
+			// Search next parameter
+			var_end = buf.find('&', var_pos);
+
+			if (std::string::npos == var_end) 
+			{
+				if (dr->full_size != dr->recv_total) 
+				{
+					dr->left = buf.size() - var_pos;
+					return true;
+				}
+			}
+
+			// Search parameter value
+			size_t delimiter = buf.find('=', var_pos);
+
+			if (delimiter >= var_end) 
+			{
+				// Get parameter name
+				std::string var_name = Utils::urlDecode(
+					buf.substr(
+						var_pos,
+						std::string::npos != var_end
+							? var_end - var_pos
+							: std::string::npos
+					)
+				);
+
+				// Store parameter with empty value
+				rd->incoming_data.emplace(
+					std::move(var_name),
+					std::string()
+				);
+			} 
+			else 
+			{
+				// Get parameter name
+				std::string var_name = Utils::urlDecode(
+					buf.substr(
+						var_pos,
+						delimiter - var_pos
+					)
+				);
+
+				++delimiter;
+
+				// Get parameter value
+				std::string var_value = Utils::urlDecode(
+					buf.substr(
+						delimiter,
+						std::string::npos != var_end
+							? var_end - delimiter
+							: std::string::npos
+					)
+				);
+
+				// Store parameter and value
+				rd->incoming_data.emplace(
+					std::move(var_name),
+					std::move(var_value)
+				);
+			}
+		}
+
+		return true;
+	}
+}
---
+++ src/server/data-variant/MultipartFormData.h
@@ -0,0 +1,31 @@
+#ifndef __MULTIPARTFORMDATA_H__
+#define __MULTIPARTFORMDATA_H__
+
+#include "Abstract.h"
+
+namespace DataVariant
+{
+
+	class MultipartFormData : public Abstract
+	{
+	public:
+			MultipartFormData() noexcept;
+	public:
+	
+		virtual void *createStateStruct(
+			const Transfer::request_data *rd,
+			const std::unordered_map<std::string, std::string> &contentParams
+		) const override;
+
+		virtual bool parse(
+			const std::string &buf,
+			Transfer::request_data *rd,
+			DataReceiver *dr
+		) const override;
+
+		virtual void destroyStateStruct(void *st) const noexcept override;
+			
+	};
+}
+
+#endif
